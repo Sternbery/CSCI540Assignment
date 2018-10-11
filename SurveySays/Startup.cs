34eafa37012bff9b247ffa5ffaffd395b1ec9895
Stefan Sternberg <stefans@EMAIL.USCUPSTATE.EDU>
2018-10-11 16:31:37 -0400

    First Commit

---
+++ SurveySays/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurveySays.Startup))]
+namespace SurveySays
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
